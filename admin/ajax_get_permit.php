c109fa85b03bd1eb1ba703c3ba808609bb8394d0
prolin99 <you@example.com>
2014-10-08 14:48:49 +0800

    指定上層目錄，設成上層權限

--- admin/ajax_get_permit.php
+++ admin/ajax_get_permit.php
@@ -2,14 +2,23 @@
 /*-----------引入檔案區--------------*/
 include_once "header.php";
 include_once "../function.php";
-
+ 
 /*-----------function區--------------*/
-
+ $catalog = array(1,2,3) ;
+$catalog_up= array(1) ;
+ 
 //上層權限
 $of_cat_sn = intval($_GET['of_cat_sn']) ;
-if ($of_cat_sn ) {
-  $data['catalog'] = getItem_Permissions($of_cat_sn , 'catalog' ) ;
-  $data['catalog_up'] = getItem_Permissions($of_cat_sn , 'catalog_up' ) ;
-  //var_dump ($data) ;
-  echo json_encode($data,JSON_FORCE_OBJECT);
-}
+ if ($of_cat_sn ) {
+   $catalog = getItem_Permissions($of_cat_sn , 'catalog' ) ;
+  $catalog_up = getItem_Permissions($of_cat_sn , 'catalog_up' ) ;
+ }
+ 
+$data['catalog'] = join(','  , $catalog ) ;
+$data['catalog_up'] = join(',' , $catalog_up ) ;
+/*
+$data['catalog'] =  $catalog  ;
+$data['catalog_up']  = $catalog_up  ;
+*/
+echo json_encode($data,JSON_FORCE_OBJECT);  
+ 
