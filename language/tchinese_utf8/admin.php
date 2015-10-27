577cb4a6bd9c7963bd98ef69ff59964bb8d87d37
Tad <tad0616@gmail.com>
2015-10-27 14:01:45 +0800

    修正tadtools路徑

--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../../tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
-define('_TAD_NEED_TADTOOLS', ' 需要 modules/tadtools，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
+define('_TAD_NEED_TADTOOLS', ' 需要 modules/tadtools，可至<a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank">XOOPS輕鬆架</a>下載。');
 
 //power.php
 define('_MA_TADUP_DB_ERROR1', '無法取得tad_uploader資料');
