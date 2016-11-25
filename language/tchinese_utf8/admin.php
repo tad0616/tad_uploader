1eec6e1f7bfd94a9e78aa02a8ef301b1cda0c84f
mambax7 <mambax7@gmail.com>
2016-11-25 17:04:51 -0500

    using XOOPS_ROOT_PATH

--- language/tchinese_utf8/admin.php
+++ language/tchinese_utf8/admin.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ . '/../../tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
+include_once XOOPS_ROOT_PATH . '/modules/tadtools/language/' . $xoopsConfig['language'] . '/admin_common.php';
 define('_TAD_NEED_TADTOOLS', ' 需要 modules/tadtools，可至<a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank">XOOPS輕鬆架</a>下載。');
 
 //power.php
