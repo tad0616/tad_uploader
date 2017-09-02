f5c7a97e2a6469759c2964423089853691a28e9f
mambax7 <mambax7@gmail.com>
2017-09-02 04:16:27 -0400

    Type casting

--- admin/ajax_get_permit.php
+++ admin/ajax_get_permit.php
@@ -8,7 +8,7 @@ $tad_uploader    = array(1, 2, 3);
 $tad_uploader_up = array(1);
 
 //上層權限
-$of_cat_sn = intval($_GET['of_cat_sn']);
+$of_cat_sn = (int)$_GET['of_cat_sn'];
 if ($of_cat_sn) {
     $tad_uploader    = getItem_Permissions($of_cat_sn, 'catalog');
     $tad_uploader_up = getItem_Permissions($of_cat_sn, 'catalog_up');
--- admin/save_sort.php
+++ admin/save_sort.php
@@ -2,8 +2,8 @@
 /*-----------引入檔案區--------------*/
 include "../../../include/cp_header.php";
 
-$cat_sn = intval($_POST['cat_sn']);
-$sort   = intval($_POST['sort']);
+$cat_sn = (int)$_POST['cat_sn'];
+$sort   = (int)$_POST['sort'];
 $sql    = "update " . $xoopsDB->prefix("tad_uploader") . " set `cat_sort`='{$sort}' where cat_sn='{$cat_sn}'";
 $xoopsDB->queryF($sql) or die("Save Sort Fail! (" . date("Y-m-d H:i:s") . ")");
 
