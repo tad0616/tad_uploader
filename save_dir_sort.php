661082c089de6cdf6d51313a4e0679e9fa1c9e19
tad <tad0616@gmail.com>
2019-03-24 13:21:54 +0800

    修正資料夾無法排序問題

--- save_dir_sort.php
+++ save_dir_sort.php
@@ -2,6 +2,7 @@
 /*-----------引入檔案區--------------*/
 include_once __DIR__ . '/header.php';
 
+$updateRecordsArray = $_POST['tr'];
 $sort = 1;
 foreach ($updateRecordsArray as $recordIDValue) {
     $sql = 'update ' . $xoopsDB->prefix('tad_uploader') . " set `cat_sort`='{$sort}' where `cat_sn`='{$recordIDValue}'";
